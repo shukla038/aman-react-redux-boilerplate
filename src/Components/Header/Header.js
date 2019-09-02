5414ba64b507005d358b71dd76328a7dac6587a0
Aman <servify@Sl-sp-lap-0331.local>
2019-09-02 08:06:48 +0530

    end to end

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './style.scss';
 
 class Header extends React.Component {
   render() {
     return (
-        <div class="header">
-            <a class="active" onClick={ ()=> this.props.history.push('/home')}>Home</a>
-            <div class="header-right">
-                <a onClick={ ()=> this.props.history.push('/')}>Logout</a>
+        <div className='header'>
+            <a href className='active' onClick={ ()=> this.props.history.push('/home')}>Home</a>
+            <div className='header-right'>
+                <a href onClick={ ()=> this.props.history.push('/')}>Logout</a>
             </div>
         </div>
     );
